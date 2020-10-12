c4aba17edaf361a029186f792ac1839fc6fd4997
niaz151 <niaz151@gmail.com>
2020-10-12 03:19:25 -0400

    Added Basic Wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      test
+      <form>
+      <div className="radio">
+        <label><input type="radio" name="optradio" checked/> Option 1</label>
+      </div>
+      <div className="radio">
+        <label><input type="radio" name="optradio" checked/> Option 1</label>
+      </div>
+      <div className="radio">
+        <label><input type="radio" name="optradio" checked/> Option 1</label>
+      </div>
+      </form>
     </div>
   );
 }
