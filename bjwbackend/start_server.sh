f621a1c3769b8edea8dae55cc575543ffc1c090a
SammyG <geller.sammy@gmail.com>
2020-10-16 21:07:18 -0400

    update server script

--- bjwbackend/start_server.sh
+++ bjwbackend/start_server.sh
@@ -2,6 +2,6 @@
 
 # starting in /var/www/bjwsubmissions/backend
 # install pm2 manually if spinning up a new instance
-pm2 stop all;
-npm install --only=production;
-pm2 start build/index.js;
+pm2 kill
+npm install --only=production
+pm2 start build/index.js
