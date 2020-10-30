3999414fed62ec6d01da709becdf93aa945427cf
SammyG <geller.sammy@gmail.com>
2020-10-30 10:48:34 -0400

    some refactoring and basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Space } from "antd";
+import { Button, Layout, Space } from "antd";
 import React from "react";
 import {
   BrowserRouter,
@@ -7,7 +7,11 @@ import {
   Redirect,
   NavLink,
 } from "react-router-dom";
-import { LoadingOutlined, WarningOutlined, UserOutlined} from "@ant-design/icons";
+import {
+  LoadingOutlined,
+  WarningOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import { signOut, useUser } from "services/api";
 import "./App.css";
 
@@ -18,13 +22,15 @@ import {
   Login as LoginPage,
   Register as RegisterPage,
   PasswordReset as PasswordResetPage,
-  Orders as OrdersPage
+  Orders as OrdersPage,
 } from "pages";
 
-import {PublicRoute, PrivateRoute} from 'components'
+import { PublicRoute, PrivateRoute } from "components";
 
 const App = () => {
   const [user, userLoading, userError] = useUser();
+
+  const { Header, Content } = Layout;
 
   const handleSignOut = async () => {
     await signOut().then(() => {
@@ -36,44 +42,47 @@ const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Space align="center" size="middle">
-          <NavLink to="/">
-            <Button>Home</Button>
-          </NavLink>
-          <NavLink to="/submit">
-            <Button>Submit</Button>
-          </NavLink>
-          <NavLink to="/orders">
-            <Button>Orders</Button>
-          </NavLink>
-          {userLoading ? (
-            <LoadingOutlined />
-          ) : user ? (
-            <>
-            <UserOutlined/><p>Hi, {user.email}</p>
-            <Button onClick={handleSignOut} style={{ marginTop: 16 }}>
-              Logout
-            </Button>
-            </>
-          ) : (
-            <NavLink to="/login">
-              <Button style={{ marginTop: 16 }}>Login</Button>
+        <Space direction="vertical">
+          <Space align="center" size="middle" direction="horizontal">
+            <NavLink to="/">
+              <Button>Home</Button>
             </NavLink>
-          )}
-          {userError && <WarningOutlined />}
+            <NavLink to="/submit">
+              <Button>Submit</Button>
+            </NavLink>
+            <NavLink to="/orders">
+              <Button>Orders</Button>
+            </NavLink>
+            {userLoading ? (
+              <LoadingOutlined />
+            ) : user ? (
+              <>
+                <UserOutlined />
+                <p>Hi, {user.email}</p>
+                <Button onClick={handleSignOut} style={{ marginTop: 16 }}>
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <NavLink to="/login">
+                <Button style={{ marginTop: 16 }}>Login</Button>
+              </NavLink>
+            )}
+            {userError && <WarningOutlined />}
+          </Space>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <PublicRoute exact path="/login" component={LoginPage} />
+            <PublicRoute exact path="/register" component={RegisterPage} />
+            <Route exact path="/passwordreset" component={PasswordResetPage} />
+            <Route exact path="/submit" component={SubmissionsPage} />
+            <PrivateRoute exact path="/orders" component={OrdersPage} />
+            <Route exact path="/invoice/:orderID" component={InvoicePage} />
+            <Route exact path="*">
+              <Redirect to="/" />
+            </Route>
+          </Switch>
         </Space>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <PublicRoute exact path="/login" component={LoginPage} />
-          <PublicRoute exact path="/register" component={RegisterPage} />
-          <Route exact path="/passwordreset" component={PasswordResetPage} />
-          <Route exact path="/submit" component={SubmissionsPage} />
-          <PrivateRoute exact path="/orders" component={OrdersPage}/>
-          <Route exact path="/invoice/:orderID" component={InvoicePage} />
-          <Route exact path="*">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
       </BrowserRouter>
     </div>
   );
--- src/pages/orders/OrdersPage.tsx
+++ src/pages/orders/OrdersPage.tsx
@@ -1,14 +1,23 @@
-import { Card, Collapse } from "antd";
-import React from "react";
+import { Button, Card, Collapse, Form, Input, Space } from "antd";
+import { Store } from "antd/lib/form/interface";
+import React, { useState } from "react";
 import { useOrders, useUser } from "services/api";
 
 const OrdersPage = () => {
   const [user] = useUser();
   const [orders, ordersLoading, ordersError] = useOrders();
+  const [isEditing, setEditing] = useState(false);
+  const isAdmin = user?.email === "blackjadedwolf@aol.com";
 
-  const userOrders = user?.email === "blackjadedwolf@aol.com" ? orders : orders?.filter((order) => order.email === user?.email);
+  const userOrders = isAdmin
+    ? orders
+    : orders?.filter((order) => order.email === user?.email);
 
-  const {Panel} = Collapse;
+  const { Panel } = Collapse;
+
+  const onFinish = (values: Store) => {
+    console.log(values);
+  };
 
   return (
     <>
@@ -18,17 +27,59 @@ const OrdersPage = () => {
           userOrders.map((order) => {
             return (
               <>
-                <p>
-                  {order.id} | {order.email} | {order.lastName} |{" "}
-                  {order.firstName}
-                </p>
-                <Collapse>
-                <Panel header="Cards" key="1">
-                  {order.cards.map((card) => {
-                    return <Card type="inner">{card.player_name}</Card>;
-                  })}
-                  </Panel>
-                </Collapse>
+                <Form onFinish={onFinish}>
+                  <Space direction="horizontal">
+                    <Form.Item name="id">
+                      <Input disabled defaultValue={order.id} />
+                    </Form.Item>
+                    <Form.Item name="email">
+                      <Input readOnly defaultValue={order.email} />
+                    </Form.Item>
+                    <Form.Item name="lastName">
+                      <Input
+                        readOnly={!isEditing}
+                        defaultValue={order.lastName}
+                      />
+                    </Form.Item>
+                    <Form.Item name="firstName">
+                      <Input
+                        readOnly={!isEditing}
+                        defaultValue={order.firstName}
+                      />
+                    </Form.Item>
+                    <Collapse>
+                      <Panel header="Cards" key="1">
+                        {order.cards.map((card) => {
+                          return <Card type="inner">{card.player_name}</Card>;
+                        })}
+                      </Panel>
+                    </Collapse>
+                    {isEditing ? (
+                      <>
+                        <Button type="primary" htmlType="submit">
+                          Save
+                        </Button>
+                        <Button
+                          type="default"
+                          htmlType="reset"
+                          onClick={() => {
+                            setEditing(false);
+                          }}
+                        >
+                          Cancel
+                        </Button>
+                      </>
+                    ) : (
+                      <Button
+                        onClick={() => {
+                          setEditing(true);
+                        }}
+                      >
+                        Edit Order
+                      </Button>
+                    )}
+                  </Space>
+                </Form>
               </>
             );
           })
