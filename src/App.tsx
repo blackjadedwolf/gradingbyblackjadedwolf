55b3a17b29347480c032bad35d9f03e38257736f
niaz151 <niaz151@gmail.com>
2020-11-03 19:53:53 -0500

    Create Header

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   Redirect,
   NavLink,
 } from "react-router-dom";
+
 import { signOut, useUser } from "services/api";
 
 import {
@@ -18,9 +19,10 @@ import {
   Orders as OrdersPage,
 } from "pages";
 
-import { PublicRoute, PrivateRoute } from "components";
+import { PublicRoute, PrivateRoute, Header } from "components";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { Person, ExclamationTriangle, Clock } from "react-bootstrap-icons";
+import './assets/styles/theme_styles.css';
 
 const App = () => {
   const [user, userLoading, userError] = useUser();
@@ -34,55 +36,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Container fluid>
-          <Col>
-            <Row>
-              <NavLink to="/">
-                <Button>Home</Button>
-              </NavLink>
-              <NavLink to="/submit">
-                <Button>Submit</Button>
-              </NavLink>
-              <NavLink to="/orders">
-                <Button>Orders</Button>
-              </NavLink>
-              {userLoading ? (
-                <Clock />
-              ) : user ? (
-                <>
-                  <Person />
-                  <p>Hi, {user.email}</p>
-                  <Button onClick={handleSignOut} style={{ marginTop: 16 }}>
-                    Logout
-                  </Button>
-                </>
-              ) : (
-                <NavLink to="/login">
-                  <Button style={{ marginTop: 16 }}>Login</Button>
-                </NavLink>
-              )}
-              {userError && <ExclamationTriangle />}
-            </Row>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <PublicRoute exact path="/login" component={LoginPage} />
-              <PublicRoute exact path="/register" component={RegisterPage} />
-              <Route
-                exact
-                path="/passwordreset"
-                component={PasswordResetPage}
-              />
-              <Route exact path="/submit" component={SubmissionsPage} />
-              <PrivateRoute exact path="/orders" component={OrdersPage} />
-              <Route exact path="/invoice/:orderID" component={InvoicePage} />
-              <Route exact path="*">
-                <Redirect to="/" />
-              </Route>
-            </Switch>
-          </Col>
-        </Container>
-      </BrowserRouter>
+      <Header/>
     </div>
   );
 };
