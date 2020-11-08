ea39cc4e0637958d2c2de46163680ad6d700fe53
niaz151 <niaz151@gmail.com>
2020-11-08 12:28:27 -0500

    Style Changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,19 +4,22 @@ const About = () => {
   return(
     <section className="page-section bg-primary text-white mb-0" id="custom-about">
       <div className="container">
-        <h2 className="page-section-heading text-center text-uppercase text-white">About</h2>
-        <div className="divider-custom divider-light">
+        <h2 className="page-section-heading text-center text-uppercase">About</h2>
+        <div className="divider-custom">
             <div className="divider-custom-line"></div>
-            <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
             <div className="divider-custom-line"></div>
         </div>
         <div className="row">
-            <div className="col-lg-4 ml-auto"><p className="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-            <div className="col-lg-4 mr-auto"><p className="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+            <div className="col-lg-12 about-text"><p className="lead">
+              BlackJadedWolf is NYC High End Marketer of one of a kind rare inserts and parallels. We have been in 
+              Business for 15 years. We buy, sell and give recommendations on all modern cards. With all the Knowledge
+              and Experience we have gain an investor, We are taking the next step in helping the community. We are
+              blessed to be accepted into the PSA Group Submission Program. We are here to help you along the way.
+            </p></div>
         </div>
         <div className="text-center mt-4">
-            <a className="btn btn-xl btn-outline-light" href="#">
-                    Sample Button
+            <a className="btn btn-xl custom-about-btn" href="https://blackjadedwolf.com">
+              Learn More
             </a>
         </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
+import logo from '../assets/img/logo.png';
 
 const Hero = () => {
   return(
     <header className="masthead bg-primary text-white text-center" id="custom_masthead">
       <div className="container d-flex align-items-center flex-column">
-        <img className="masthead-avatar mb-5" src="../assets/img/logo.png" alt="" />
-        <h1 className="masthead-heading text-uppercaset mb-0">Grading by BlackJadedWolf</h1>
-        <div className="divider-custom divider-light"></div>
-        <p className="masthead-subheading font-weight-light mb-0">Card Grading and Consignment</p>
+        <img className="masthead-avatar mb-5 custom-img" src={logo} alt="" />
     </div>
     </header>
   )
