b5bc4fe7527961b6426167b1bd3eaac30d3254a2
niaz151 <niaz151@gmail.com>
2020-11-27 16:33:45 -0500

    Style Changes

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -20,12 +20,17 @@ const Home = () => {
             <div className="divider-custom-line"></div>
         </div>
         <div className="row">
-            <div className="col-lg-12 about-text"><p className="lead">
-              BlackJadedWolf is NYC High End Marketer of one of a kind rare inserts and parallels. We have been in 
-              Business for 15 years. We buy, sell and give recommendations on all modern cards. With all the Knowledge
-              and Experience we have gain an investor, We are taking the next step in helping the community. We are
-              blessed to be accepted into the PSA Group Submission Program. We are here to help you along the way.
-            </p></div>
+            <div className="col-lg-12 about-text">
+              <p className="lead">
+                BlackJadedWolf is a high end marketer of one of a kind rare inserts and parallels based in NYC. 
+                
+                We are partnered with the PSA Group Submission Program and have been in business for 15 years.
+
+                Via our client services we help people buy, sell, and give reccomendations on all modern cards.
+                
+                We're here to help you along your way as you step into the collectible community. 
+              </p>
+            </div>
         </div>
         <div className="text-center mt-4">
             <a className="btn btn-xl custom-about-btn" href="https://blackjadedwolf.com">
