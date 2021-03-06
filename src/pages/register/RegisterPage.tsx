8c8cbf144e43f276bdafa2934379255551d5e443
niaz151 <niaz151@gmail.com>
2021-03-06 16:14:02 -0500

    add unstyled name/phone input to register page

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -7,6 +7,9 @@ const RegisterPage = () => {
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
   const [confirmPassword, setConfirmPassword] = useState<string>();
+  const [fName, setfName] = useState<string>();
+  const [lName, setlName] = useState<string>();
+  const [phoneNum, setphoneNum] = useState<string>();
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     if (!email || !password) {
@@ -21,25 +24,89 @@ const RegisterPage = () => {
 
   return (
     <Form onSubmit={onSubmit} style={FormStyles}>
+
       <div className="login-header">
         Create An Account
       </div>
-      <Form.Group controlId="email">
+
+      <Form.Group controlId="f_name">
+
         <Form.Control
           required
           style={{width:"20rem", marginTop:"2rem"}}
+          type="First_Name"
+          placeholder="First Name"
+          onChange={(event) => {
+            setfName(event.target.value);
+          }}
+        />
+
+        <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Enter your first name
+        </Form.Control.Feedback>
+
+      </Form.Group>
+
+      <Form.Group controlId="l_name">
+
+        <Form.Control
+          required
+          style={{width:"20rem"}}
+          type="Last_Name"
+          placeholder="Last Name"
+          onChange={(event) => {
+            setlName(event.target.value);
+          }}
+        />
+
+        <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Enter your last name
+        </Form.Control.Feedback>
+
+      </Form.Group>
+
+      <Form.Group controlId="phone_number">
+
+        <Form.Control
+          required
+          style={{width:"20rem"}}
+          type="Phone_Number"
+          placeholder="Phone Number"
+          onChange={(event) => {
+            setphoneNum(event.target.value);
+          }}
+        />
+
+        <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Enter your phone number
+        </Form.Control.Feedback>
+
+      </Form.Group>
+
+
+      <Form.Group controlId="email">
+
+        <Form.Control
+          required
+          style={{width:"20rem"}}
           type="email"
           placeholder="Email"
           onChange={(event) => {
             setEmail(event.target.value);
           }}
         />
+
         <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
         <Form.Control.Feedback type="invalid">
           Please enter your email
         </Form.Control.Feedback>
+
       </Form.Group>
       <Form.Group controlId="password">
+
         <Form.Control
           required
           style={{width:"20rem"}}
@@ -49,12 +116,15 @@ const RegisterPage = () => {
             setPassword(event.target.value);
           }}
         />
+
         <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
         <Form.Control.Feedback type="invalid">
           Please enter your password
         </Form.Control.Feedback>
+
       </Form.Group>
       <Form.Group controlId="confirmPassword">
+
         <Form.Control
           required
           style={{width:"20rem"}}
@@ -65,10 +135,12 @@ const RegisterPage = () => {
             setConfirmPassword(event.target.value);
           }}
         />
+
         <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
         <Form.Control.Feedback type="invalid">
           Please confirm your password
         </Form.Control.Feedback>
+
       </Form.Group>
       <div style={BtnWrap}>
         <Row>
@@ -82,6 +154,7 @@ const RegisterPage = () => {
           </Col>
         </Row>
       </div>
+
     </Form>
   );
 };
