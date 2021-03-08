51fe09809d4832c575ed5eb968152233d6f5a0c7
niaz151 <niaz151@gmail.com>
2021-03-07 20:18:08 -0500

    new form

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -14,44 +14,50 @@ const ProfilePage = () => {
   return(
     <div className="profile-wrap">
       <div className="container profile-container border border-primary">
-      <form>
-      <div className="form-row">
-      <div className="form-group col-md-6">
-        <label>Email</label>
-        <input type="email" className="form-control" id="email" placeholder="Email"/>
-      </div>
-    <div className="form-group col-md-6">
-      <label>Password</label>
-      <input type="password" className="form-control" id="password4" placeholder="Password"/>
-    </div>
-  </div>
-  <div className="form-group">
-    <label>Address</label>
-    <input type="text" className="form-control" id="address" placeholder="1234 Main St"/>
-  </div>
-  <div className="form-group">
-    <label>Phone Number</label>
-    <input type="text" className="form-control" id="phoneNum" placeholder="Phone Number"/>
-  </div>
-  <div className="form-row">
-    <div className="form-group col-md-6">
-      <label>City</label>
-      <input type="text" className="form-control" id="city"/>
-    </div>
-    <div className="form-group col-md-4">
-      <label>State</label>
-      <select id="state" className="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div className="form-group col-md-2">
-      <label>Zip</label>
-      <input type="text" className="form-control" id="zip"/>
-    </div>
-  </div>
-  <button type="submit" className="btn btn-primary">Save</button>
-</form>
+        <form>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label>First Name</label>
+              <input type="f_name" className="form-control" id="f_name" placeholder="First Name"/>
+            </div>
+            <div className="form-group col-md-6">
+              <label>Last Name</label>
+              <input type="l_name" className="form-control" id="l_name" placeholder="Last Name"/>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label>Email</label>
+              <input type="email" className="form-control" id="email" placeholder="Email"/>
+            </div>
+            <div className="form-group col-md-6">
+              <label>Password</label>
+              <input type="password" className="form-control" id="password" placeholder="Password"/>
+            </div>
+          </div>
+          <div className="form-group">
+            <label>Phone Number</label>
+            <input type="text" className="form-control" id="phoneNum" placeholder="Phone Number"/>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label>City</label>
+              <input type="text" className="form-control" id="city"/>
+            </div>
+            <div className="form-group col-md-4">
+              <label>State</label>
+              <select id="state" className="form-control">
+                <option selected>Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="form-group col-md-2">
+              <label>Zip</label>
+              <input type="text" className="form-control" id="zip"/>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">Save</button>
+        </form>
       </div>
     </div>
   )
